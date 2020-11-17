6f07f5b05a61f50bcc48faf9232ad0cd802163be
Lucas Passos <lucas.depassos@yahoo.com.br>
2020-11-17 13:51:42 -0300

    images fix

---
+++ src/components/Infosection/data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Soluções de T.I Premium',
+    headline: 'Seus sonhos com potenciais ilimitados',
+    description: 'Baixe o nosso E-book e entenda como as soluções de tecnologia podem destacar o seu produto no mercado',
+    buttonLabel: 'Baixar E-book',
+    imgStart: false,
+    img: require('../../images/undraw2.png'),
+    alt: 'img1',
+    dark: true,
+    primary: true,
+    darkText: false
+}
